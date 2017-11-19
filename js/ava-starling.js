87b7a4087ac018657d0c1b82391ec033e2922d90
David Forster <david@davidforster.com>
2017-11-19 16:46:45 +0000

    Adding custom JS

---
+++ js/ava-starling.js
@@ -0,0 +1,15 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Update Kindle links so that GB users are sent to the correct Amazon URL
+  $.get("https://ipinfo.io", function(response) {
+    // console.log(response);
+    if (response.country === 'GB') {
+      $("a.kindle")
+        .each(function() {
+          this.href = this.href = 'http://amzn.to/2zWwRxs';
+        });
+    }
+  }, "jsonp");
+
+})(jQuery); // End of use strict
